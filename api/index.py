c169b114265895c164ca618e6a81fad56fb2c3e0
HitKoladiya <104366639+HitKoladiya@users.noreply.github.com>
2024-03-02 19:01:51 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    return 'loda Sanu muno pogi jaje nakkar ricksha ma aavavu padase, CHARUSAT ni placement policy joi nathi and bhai ne T&C jovi chhe.'
 
 @app.route('/about')
 def about():
